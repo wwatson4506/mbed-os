1cadb9fe47bf1c6f5001a49a2aaf1f37dc543063
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2023-01-21 17:00:38 -0800

    pyocd working!

--- tools/cmake/upload_methods/UploadMethodPYOCD.cmake
+++ tools/cmake/upload_methods/UploadMethodPYOCD.cmake
@@ -46,10 +46,18 @@ set(UPLOAD_GDBSERVER_DEBUG_COMMAND
 	${Python3_EXECUTABLE}
 	-m pyocd
 	gdbserver
-	-v
 	--no-wait
 	-t ${PYOCD_TARGET_NAME}
 	${PYOCD_PROBE_ARGS}
 	-f ${PYOCD_CLOCK_SPEED}
-	-p ${GDB_PORT}
-	--semihosting)
+	-p ${GDB_PORT})
+
+# Reference: https://github.com/Marus/cortex-debug/blob/056c03f01e008828e6527c571ef5c9adaf64083f/src/pyocd.ts#L40
+set(UPLOAD_LAUNCH_COMMANDS
+"monitor reset halt"
+"load"
+"break main"
+)
+set(UPLOAD_RESTART_COMMANDS
+"monitor reset"
+)
